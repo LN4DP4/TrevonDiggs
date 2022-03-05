c89757959e7b777e8681ca1fac9435ebfcc67970
Lowseyboy <99146511+LN4DP4@users.noreply.github.com>
2022-03-05 22:09:56 +0000

    Simple nav

---
+++ Website/Homepage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="webstyle.css">
+    <title>Document</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark"> <!-- Navigation Bar -->
+  <div class="container-fluid">
+    <a class="navbar-brand" href="javascript:void(0)"> <!-- Logo -->
+        <img src="logo-social.png" alt="Company Logo" style="width:40px;" class="rounded"> 
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mynavbar">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Contact Us</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <button class="btn btn-primary "  type="button">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid mt-3">
+  
+</div>
+</body>
+</html>
